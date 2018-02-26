4de51aec7078bebc807b481ee9711102c0fdbc53
Conor Flynn <conor.j.flynn@gmail.com>
2018-02-26 17:55:28 +0000

    Moves pushing of a datapoint to use message passing.

---
+++ src/core/background.js
@@ -0,0 +1,64 @@
+/**
+ * Actually pushes the datapoint to the lambda function.
+ */
+chrome.runtime.onMessage.addListener(
+  function(datapointDetails, sender, callback) {
+    datasource = datapointDetails['ds'];
+    username = datapointDetails['username'];
+    projects = datapointDetails['projects'];
+    datapoint = datapointDetails['datapoint'];
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://push.metro.exchange";
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    var data = {
+      "datasource": datasource,
+      "projects": projects,
+      "timestamp": Date.now(),
+      "data": datapoint,
+      "user": username
+    };
+
+    console.log("Pushing: ");
+    console.log(data);
+
+    // Only send datapoint to lambda if not in dev mode:
+    chrome.storage.sync.get("Settings-devModeCheckbox", function(items) {
+      if(chrome.runtime.error) {
+        return false;
+      } else {
+        if(items["Settings-devModeCheckbox"]) {
+          console.log("Not publishing the datapoint as running in dev mode.");
+        } else {
+          // Push with the API key:
+          getKeyAndPush(xhr, JSON.stringify(data));
+        }
+      }
+    });
+  }
+);
+
+/*
+ * Gets the API Gateway key and pushed the data with it.
+ */
+const getKeyAndPush = function(xhr, data) {
+  let keyRequester = new XMLHttpRequest();
+  keyRequester.open("GET", "https://metro.exchange/api/profile/api_key/", true);
+
+  keyRequester.onreadystatechange = function() {
+    if(keyRequester.readyState == 4) {
+      keyResponse = JSON.parse(keyRequester.responseText);
+      apiKey = keyResponse['content']['key'];
+
+      xhr.setRequestHeader("X-API-Key", apiKey);
+
+      console.log("Pushing data with API key.");
+      xhr.send(data);
+    }
+  }
+
+  keyRequester.send();
+}
--- src/core/scriptLoader.js
+++ src/core/scriptLoader.js
@@ -13,64 +13,6 @@ const sameJSONStructure = function (o1, o2) {
 }
 
 /**
- * Actually pushes the datapoint to the lambda function.
- */
-const pushDatapoint = function(datasource, username, projects, datapoint) {
-  var xhr = new XMLHttpRequest();
-  var url = "https://push.metro.exchange";
-
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-type", "application/json");
-
-  var data = {
-    "datasource": datasource,
-    "projects": projects,
-    "timestamp": Date.now(),
-    "data": datapoint,
-    "user": username
-  };
-
-  console.log("Pushing: ");
-  console.log(data);
-
-  // Only send datapoint to lambda if not in dev mode:
-  chrome.storage.sync.get("Settings-devModeCheckbox", function(items) {
-    if(chrome.runtime.error) {
-      return false;
-    } else {
-      if(items["Settings-devModeCheckbox"]) {
-        console.log("Not publishing the datapoint as running in dev mode.");
-      } else {
-        // Push with the API key:
-        getKeyAndPush(xhr, JSON.stringify(data));
-      }
-    }
-  });
-}
-
-/*
- * Gets the API Gateway key and pushed the data with it.
- */
-const getKeyAndPush = function(xhr, data) {
-  let keyRequester = new XMLHttpRequest();
-  keyRequester.open("GET", "https://metro.exchange/api/profile/api_key/", true);
-
-  keyRequester.onreadystatechange = function() {
-    if(keyRequester.readyState == 4) {
-      keyResponse = JSON.parse(keyRequester.responseText);
-      apiKey = keyResponse['content']['key'];
-
-      xhr.setRequestHeader("X-API-Key", apiKey);
-
-      console.log("Pushing data with API key.");
-      xhr.send(data);
-    }
-  }
-
-  keyRequester.send();
-}
-
-/**
  * Script to check that the schema of the datasource matches the datapoint.
  * It only validates that the keys match.
  */
@@ -107,7 +49,16 @@ const runDataSource = function(datasource, scriptURL, schemaURL, projects, DS, u
         sendDatapoint: function(datapoint) {
           if(validateDatapoint(schema, datapoint)) {
             console.log("Pushing datapoint for " + DS);
-            pushDatapoint(DS, username, projects, datapoint);
+            let datapointDetails = {
+              'ds': DS,
+              'username': username,
+              'projects': projects,
+              'datapoint': datapoint
+            }
+
+            chrome.runtime.sendMessage(datapointDetails, function(response) {
+              console.log(response);
+            });
           }
         },
 
